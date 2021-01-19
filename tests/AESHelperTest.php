634a2f8fda1dc3a95509a2cd506c8291134bfe3a
Ali Ibrahim <aliibra395@gmail.com>
2021-01-19 07:32:00 +0200

    Added basic phpunit test

---
+++ tests/AESHelperTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ai\AESHelper\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AESHelperTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function demo_echo()
+    {
+        $this->assertTrue(true);
+    }
+}
